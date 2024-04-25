7438f31ea99318b3806caff83ded4a3b3a4c4a54
Virali Patel <patelvuh@gmail.com>
2024-04-24 19:20:24 -0700

    fixing pt 2

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -1,5 +1,5 @@
 package edu.sjsu;
-import java.util.*;
+
 public class NameChecker {
     /**
      * Checks if a given string is a valid name following these rules:
@@ -11,19 +11,16 @@ public class NameChecker {
      * @return True if input is a valid name, else false
      */
     public static boolean check(String input) {
-      if (input == null || input.isEmpty() || input.length() < 2 || input.length() > 40) {
-          return false;
-      }
-        char firstChar = input.charAt(0);
-      if (firstChar == '\'' || firstChar == '\"') {
-          return false;
-      }
-        if (!input.chars().allMatch(Character::isLetter)) {
-          return false;
-      }
-  
-      return true;
-  }
-  
+        if (input.length() < 2 || input.length() > 40) {
+            return false;
+        }
+
+        if (input.startsWith("-") || input.startsWith("'")) {
+            return false;
+        }
+
+        return input.matches("[A-Za-z]([A-Za-z]|((?<!['\\-])['\\-](?=[A-Za-z])))*[A-Za-z]");
+
+
+    }
 }
-     
